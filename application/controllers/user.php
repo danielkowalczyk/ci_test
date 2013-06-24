baf585d559c0b3f1e0cdc8b21d57265431d57563
Daniel Kowalczyk <kontakt@danielkowalczyk.eu>
2013-06-24 18:38:30 +0200

    Added static pages support

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -1,7 +1,10 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 class User extends CI_Controller {
-
+    
+    public function view() {
+        
+    }
 }
 
 ?>
